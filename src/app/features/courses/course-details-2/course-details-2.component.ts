40d25a8bf2849b5a1cdea2c77c4b744204ad9a85
Hassim506 <hassimiou061@gmail.com>
2026-01-26 19:54:41 +0100

    Feat: Ajout de la page details formations

--- src/app/features/courses/course-details-2/course-details-2.component.ts
+++ src/app/features/courses/course-details-2/course-details-2.component.ts
@@ -1,11 +1,9 @@
 // src/app/features/courses/course-details-2/course-details-2.component.ts
 
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Formation } from '../../../shared/models/formation.models';
-
-import { FormationApiResponse } from '../../../shared/models/Formations.models';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Formation } from '../../../shared/models/formation.models';
 import { FormationsService } from '../../../shared/service/Formationsss/formations.service';
 
 @Component({
@@ -17,33 +15,44 @@ import { FormationsService } from '../../../shared/service/Formationsss/formatio
 })
 export class CourseDetails2Component implements OnInit {
 
-  formation!: Formation;
-  loading = true;
+  formation: Formation | null = null;
+  loading: boolean = true;
+  error: string = '';
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private formationsService: FormationsService
   ) {}
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    if (id) {
-      this.getFormation(id);
-    }
+    this.route.params.subscribe(params => {
+      const formationId = +params['id'];
+      if (formationId) {
+        this.loadFormationDetails(formationId);
+      }
+    });
   }
 
-  getFormation(id: number) {
+  loadFormationDetails(id: number): void {
     this.loading = true;
+    this.error = '';
+
     this.formationsService.getFormationById(id).subscribe({
-      next: (res: FormationApiResponse) => {
-        this.formation = res as unknown as Formation;
+      next: (response: any) => {
+        // accepte plusieurs structures comme dans ClientDetails
+        this.formation = response.data || response.formation || response;
         this.loading = false;
       },
-      error: (err) => {
-        console.error('Erreur chargement formation', err);
+      error: () => {
+        this.error = 'Formation introuvable';
         this.loading = false;
       }
     });
+  }
+
+  goBack(): void {
+    this.router.navigate(['/formations']);
   }
 
   get totalLessons(): number {
@@ -56,5 +65,9 @@ export class CourseDetails2Component implements OnInit {
 
   get totalDuration(): number {
     return this.formation?.duree_totale || 0;
+  }
+
+  canSubscribe(): boolean {
+    return !!this.formation?.inscription_ouverte;
   }
 }
