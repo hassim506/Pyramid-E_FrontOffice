73934c6b0ecf3c9f8e8466e7a2198d865a66e034
Hassim506 <hassimiou061@gmail.com>
2026-02-04 19:48:00 +0100

    Feat:Ajout de la page de lecture des formations

---
+++ src/app/features/student/lecture-formation/lecture-formation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lecture-formation',
+  imports: [],
+  templateUrl: './lecture-formation.component.html',
+  styleUrl: './lecture-formation.component.scss',
+})
+export class LectureFormationComponent {
+
+}
